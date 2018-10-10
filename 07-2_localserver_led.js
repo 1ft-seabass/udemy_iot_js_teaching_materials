d5bc6857ef25d59de38c23c39725ac1f36e78d22
Tanaka Seigo <seigo@1ft-seabass.jp>
2018-10-10 16:44:53 +0900

    7章 COMポート削除

--- 07-2_localserver_led.js
+++ 07-2_localserver_led.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 // USBに接続されているArduino Unoボードの呼び出し
 // new five.Board() で記述すると指定なし呼び出しして自動で探します
-var board = new five.Board({ port:"COM3" });
+var board = new five.Board();
 // USBに複数デバイスが接続されているなど自動で見つからないときはArduino IDEでの指定ポートを参考に名指しで指定
 // var board = new five.Board({ port:"COM3" }); // Windowsでポートが COM3 の場合
 // var board = new five.Board({ port:"/dev/cu.usbmodem1411" }); // Macでポートが /dev/cu.usbmodem1411 の場合
